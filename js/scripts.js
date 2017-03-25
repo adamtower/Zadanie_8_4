006e3b9807af5a2eee4231f892a3d436c40daa4c
Adam <slayer@plusnet.pl>
2017-03-25 13:53:40 +0100

    Zadanie 8.4 Pole trojkąta

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a=prompt("Podaj wartość a",a),
+    h=prompt("Podaj wartość h",h),
+    triangleArea;
+triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+//console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
